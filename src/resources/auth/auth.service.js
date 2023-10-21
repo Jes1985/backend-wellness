6f109e2d8268e508a17b5be7eaa2c57f26738c94
Giovani de SOUZA <giovanidesouza999@gmail.com>
2023-10-21 21:11:59 +0100

    refactor: Use jsonResponse in aut.service

--- src/resources/auth/auth.service.js
+++ src/resources/auth/auth.service.js
@@ -2,35 +2,48 @@ const { dbConnect } = require("../../config/dbConnect");
 const User = require("../user/user.model");
 const { verifyPassword } = require("../../utils/auth.util");
 const jwt = require("jsonwebtoken");
+const { jsonResponse } = require("../../utils/jsonResponse.util");
 require("dotenv").config();
 
 class AuthService {
   async oAuthLogin(req, res, next) {
-    const { email, password } = req.body;
+    try {
+      const { email, password } = req.body;
 
-    const userLogin = await User.findOne({ email }).select("+isAdmin");
+      const userLogin = await User.findOne({ email }).select("+isAdmin");
 
-    if (!userLogin) {
-      throw new Error("Erreur: utilisateur introuvable");
+      if (!userLogin) {
+        throw new Error("Erreur: utilisateur introuvable");
+      }
+
+      const isValid = await verifyPassword(password, userLogin.password);
+
+      if (!isValid) {
+        throw new Error("Erreur: Mot de passe incorrect");
+      }
+
+      const user = {
+        id: userLogin._id,
+        username: userLogin.username,
+        email: userLogin.email,
+        isAdmin: userLogin.isAdmin,
+      };
+      const token = jwt.sign(user, process.env.JWT_ACCESS_KEY, {
+        expiresIn: "1d",
+      });
+
+      return res.json(
+        jsonResponse(
+          JSON.stringify({ ...user, token }),
+          200,
+          "User logged successfully"
+        )
+      );
+    } catch (err) {
+      return res.json(
+        jsonResponse(JSON.stringify(undefined), 400, err.message)
+      );
     }
-
-    const isValid = await verifyPassword(password, userLogin.password);
-
-    if (!isValid) {
-      throw new Error("Erreur: Mot de passe incorrect");
-    }
-
-    const user = {
-      id: userLogin._id,
-      username: userLogin.username,
-      email: userLogin.email,
-      isAdmin: userLogin.isAdmin,
-    };
-    const token = jwt.sign(user, process.env.JWT_ACCESS_KEY, {
-      expiresIn: "1d",
-    });
-
-    return { ...user, token };
   }
 }
 
